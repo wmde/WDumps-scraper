584c9a0d3c05bd1a805e63e691350c1b14e16f9e
Mona Thierse <mona.thierse@wikimedia.de>
2026-04-02 15:12:24 +0200

    add exception

--- src/wdumps_scraper/scraper.py
+++ src/wdumps_scraper/scraper.py
@@ -1,19 +1,18 @@
-import requests
+from requests.exceptions import HTTPError
+from request_cache import CachedSession
+
+
+class CacheDuration(Enum):
+    NO_CACHE = 0
+    LOW = 7200
+    HIGH = None
 
 class Scraper:
-    def __init__(self, session):
+    def __init__(self, session: CachedSession) -> None:
         self.session = session
-
-    class CacheDuration(Enum):
-        NO_CACHE = 0
-        LOW = 7200
-        HIGH = None
     
-    def get(url: str, cache_duration: CacheDuration=CacheDuration.NO_CACHE) -> str:
-        response = session.get(url, expire_after=cache_duration.value)
-        try:
-            response.raise_for_status()
-            return response.text
-        except requests.exceptions.HTTPError:
-            raise 
+    def get(self, url: str, cache_duration: CacheDuration=CacheDuration.NO_CACHE) -> str:
+        response = self.session.get(url, expire_after=cache_duration.value)
+        response.raise_for_status()
+        return response.text
 
