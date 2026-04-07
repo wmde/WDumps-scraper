d8cb0fd30ff6f8ade8a1c0a45ef4252d14528afe
Itamar Givon <itamar.givon@wikimedia.de>
2026-04-07 17:21:59 +0200

    Add pre-commit configuration and CI setup
    Also refactor code for consistency

--- src/wdumps_scraper/parsing.py
+++ src/wdumps_scraper/parsing.py
@@ -4,30 +4,34 @@ from bs4 import BeautifulSoup
 
 
 def extract_last_id(html_content: str) -> int:
-  soup = BeautifulSoup(html_content, "html.parser")
-  link_elem = soup.find("table").find("a")
-  link_url = link_elem["href"]
-  return int(link_url.split("/")[-1])
+    soup = BeautifulSoup(html_content, "html.parser")
+    table = soup.find("table")
+    a_tag = table.find("a") if table else None
+    href = str(a_tag["href"]) if a_tag else None
+    dump_id = href.split("/")[-1] if href else None
+    return int(dump_id)  # type: ignore[arg-type]
+
 
 def extract_name(html_content: str) -> str:
-  soup = BeautifulSoup(html_content, "html.parser")
-  dump_header = soup.find("h2")
-  if dump_header is not None:
-    name = dump_header.text.split(": ", maxsplit=1)[-1]
-    return name
-  else:
-    return ""
+    soup = BeautifulSoup(html_content, "html.parser")
+    dump_header = soup.find("h2")
+    if not dump_header:
+        return ""
+    return dump_header.text.split(": ", maxsplit=1)[-1]
 
-def extract_filters(html_content: str) -> dict:
-  soup = BeautifulSoup(html_content, "html.parser")
-  spec = soup.find("h2", string="Spec").find_next("pre").get_text()
-  if spec is not None:
-    dump_filters = json.loads(spec)
+
+def extract_filters(html_content: str) -> dict[str, str]:
+    soup = BeautifulSoup(html_content, "html.parser")
+    spec_tag = soup.find("h2", string="Spec")  # type: ignore[call-overload]
+    if not spec_tag:
+        return {}
+    pre = spec_tag.find_next("pre")
+    if not pre:
+        return {}
+    dump_filters = json.loads(pre.get_text())
     return {
         "labels": "yes" if dump_filters["labels"] else "no",
         "descriptions": "yes" if dump_filters["descriptions"] else "no",
         "aliases": "yes" if dump_filters["aliases"] else "no",
-        "sitelinks": "yes" if dump_filters["sitelinks"] else "no"
+        "sitelinks": "yes" if dump_filters["sitelinks"] else "no",
     }
-  else:
-      return {}
--- src/wdumps_scraper/scraper.py
+++ src/wdumps_scraper/scraper.py
@@ -6,11 +6,10 @@ from wdumps_scraper.cached_limiter_session import CachedLimiterSession, CacheDur
 class Scraper:
     def __init__(self, session: CachedLimiterSession) -> None:
         self.__session = session
-    
+
     def get(
-            self, url: str, cache_duration: CacheDuration=CacheDuration.NO_CACHE
+        self, url: str, cache_duration: CacheDuration = CacheDuration.NO_CACHE
     ) -> str:
         response = self.__session.get(url, expire_after=cache_duration.value)
         response.raise_for_status()
         return response.text
-
