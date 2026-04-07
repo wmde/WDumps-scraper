5c4d81d102765ea50fbe6d5914d7f37979833e88
Mona Thierse <mona.thierse@wikimedia.de>
2026-04-07 13:02:11 +0200

    Created cached session module

--- src/wdumps_scraper/__init__.py
+++ src/wdumps_scraper/__init__.py
@@ -1,3 +1,5 @@
 from wdumps_scraper import parsing
+from wdumps_scraper.cached_limiter_session import CachedLimiterSession, CacheDuration
+from wdumps_scraper.scraper import Scraper
 
-__all__ = ["parsing"]
+__all__ = ["parsing", "CachedLimiterSession", "CacheDuration", "Scraper"]
---
+++ src/wdumps_scraper/cached_limiter_session.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from typing import Any
+
+from requests import Session
+from requests_cache import DO_NOT_CACHE, NEVER_EXPIRE, CacheMixin
+from requests_ratelimiter import LimiterMixin
+
+__all__ = ["CachedLimiterSession", "CacheDuration"]
+
+
+class CacheDuration(Enum):
+    NO_CACHE = DO_NOT_CACHE
+    LOW = 7200
+    INDEFINITE = NEVER_EXPIRE
+
+class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
+    def __init__(self, *args: Any, user_agent: str | None =None, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        if user_agent is not None:
+            self.headers.update({"User-Agent": user_agent})
