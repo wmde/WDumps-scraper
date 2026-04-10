61c3d8ef202475861a0d7709c58f08ca0b657963
Mona Thierse <mona.thierse@wikimedia.de>
2026-04-10 12:16:56 +0200

    add reder_languages function

--- src/wdumps_scraper/rendering.py
+++ src/wdumps_scraper/rendering.py
@@ -5,3 +5,8 @@ def render_includes(spec: dict[str, Any]) -> str:
     filters = ["labels", "descriptions", "aliases", "sitelinks"]
     dump_filters = [f for f in filters if f in spec and spec[f]]
     return ", ".join(dump_filters)
+
+
+def render_languages(spec: dict[str, Any]) -> str:
+    dump_languages = [language for language in spec["languages"] if spec["languages"]]
+    return ", ".join(dump_languages)
