1e91837db5eb54300ae6e5877eff4e31774fc5c6
Mona Thierse <mona.thierse@wikimedia.de>
2026-04-30 10:21:53 +0200

    move label resolution into render function

--- src/wdumps_scraper/dumps_info_loader.py
+++ src/wdumps_scraper/dumps_info_loader.py
@@ -40,7 +40,6 @@ class DumpsInfoLoader:
         dumps = []
         struct_dumps = []
         skipped = []
-        labels: dict = {}
 
         with ThreadPoolExecutor(max_workers=self.__max_workers) as executor:
             futures = {}
@@ -59,12 +58,8 @@ class DumpsInfoLoader:
 
         wd_ids = sorted(self.__extract_ids(dumps))
 
-        if self.__wikidata_client:
-            for i in range(0, len(wd_ids), 50):
-                labels.update(self.__wikidata_client.get_labels(wd_ids[i:50]))
-
         for i in range(0, len(dumps)):
-            struct_dumps.append(self.__render(dumps[i]))
+            struct_dumps.append(self.__render(dumps[i], wd_ids))
 
         return ScrapeResult(struct_dumps, skipped)
 
@@ -104,7 +99,11 @@ class DumpsInfoLoader:
 
         return wd_ids
 
-    def __render(self, data: dict[str, Any]) -> DumpInfo:
+    def __render(self, data: dict[str, Any], ids: list) -> DumpInfo:
+        if self.__wikidata_client:
+            labels: dict = {}
+            for i in range(0, len(ids), 50):
+                labels.update(self.__wikidata_client.get_labels(ids[i:50]))
         includes = rendering.render_includes(data["spec"])
         languages = rendering.render_languages(data["spec"])
         statements = rendering.render_statement_filters(data["spec"])
